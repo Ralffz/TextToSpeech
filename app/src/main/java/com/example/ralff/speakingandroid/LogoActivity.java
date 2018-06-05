7cc3428736955a41b0dc1f2100db2940c083282f
Ralffz <Rabel141@gmail.com>
2018-06-05 11:44:17 +0300

    Initial commit

---
+++ app/src/main/java/com/example/ralff/speakingandroid/LogoActivity.java
@@ -0,0 +1,27 @@
+package com.example.ralff.speakingandroid;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LogoActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logo);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), SpeechActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+
+    }
+}
